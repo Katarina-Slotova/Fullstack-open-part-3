79bb035dab75e4bea3cdaedb36233f0b1585256e
Katarina-Slotova <katarina.slotova@gmail.com>
2022-10-26 12:06:26 +0300

    Complete exercises up to 3.8, including morgan

--- part3/index.js
+++ part3/index.js
@@ -1,8 +1,12 @@
 const { response, request } = require('express')
 const express = require('express')
+let morgan = require('morgan')
+
 const app = express()
 
 app.use(express.json())
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 let persons = [
 	{ 
